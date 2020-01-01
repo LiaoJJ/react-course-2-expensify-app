473b462882f338d7e5cee66f996d520af5f94e0b
LJJ <ljj@LJJs-MacBook-Pro.local>
2020-01-01 16:15:59 -0500

    remove dummy expense date

--- src/app.js
+++ src/app.js
@@ -12,20 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-
-store.dispatch(setTextFilter('water'));
-store.dispatch(sortByAmount());
-
-setTimeout(()=>{
-    store.dispatch(setTextFilter('bill'));
-}, 3000);
-
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filter));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
