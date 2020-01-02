1f17218aae6836b658481dd350f88b880a848c58
LJJ <ljj@LJJs-MacBook-Pro.local>
2020-01-01 21:07:27 -0500

    Setup production build and server

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store = configureStore();
+const store = configureStore(); 
 
 const jsx = (
     <Provider store={store}>
