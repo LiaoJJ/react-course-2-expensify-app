5b2ee38c273bc87440fea4fa0c15e90677c7c065
LJJ <ljj@LJJs-MacBook-Pro.local>
2020-01-02 13:02:19 -0500

    setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output:{
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
     
@@ -33,7 +33,6 @@ module.exports = (env) => {
                                 sourceMap: true
                             }
                         }
-                        
                     ]
                 })
             }]
@@ -43,8 +42,9 @@ module.exports = (env) => {
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer:{
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            contentBase: path.join(__dirname, 'public' ),
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 };
